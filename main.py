e2c270345e912b3398b5511a37c1ba8e4a2320f0
MarlevPy <marcus.levin89@gmail.com>
2018-01-19 22:07:00 +0100

    Refactoring
    Signed-off-by: MarlevPy <marcus.levin89@gmail.com>

--- main.py
+++ main.py
@@ -1,312 +1,62 @@
 #!/usr/bin/python3
 # -*- coding: iso-8859-1 -*-
-import kivy
+import json
+
+from kivy import require
 from kivy.app import App
-from kivy.clock import Clock
-from kivy.uix.label import Label
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.anchorlayout import AnchorLayout
-from kivy.uix.image import Image
-from kivy.uix.button import Button, ButtonBehavior
-from kivy.uix.popup import Popup
-from kivy.graphics import Color, Rectangle
-from kivy.uix.screenmanager import Screen, NoTransition
-from kivy.properties import StringProperty, BooleanProperty, NumericProperty, ObjectProperty
+from kivy.lang import Builder
+from kivy.uix.screenmanager import NoTransition
+from kivy.properties import NumericProperty, ObjectProperty
 from kivy.config import Config
 
-kivy.require('1.10.0')
+from widgets import PlayersScoreLayout, GridLabelButton, GridButton,\
+    QuestionScreen, AnswerScreen, PopupPlayerLayout, PopupPlayerContent,\
+    PopupBaseLayout, PopupButtonLayout, AddPointsPopup
+
+require('1.10.0')
 Config.set('graphics', 'fullscreen', 'auto')
 
 PLAYERS = ('Lag 1', 'Lag 2', 'Lag 3')
 
-# TODO: Add path for question image and answer image to JEOPARDY_DICT
-
-JEOPARDY_DICT = {
-    'Nyårskunskap': {
-        10: {'title': 'Nyårscuisine',
-             'question': 'God mat hör nyårsafton till.\n\nDet är inte alltid lätt att förstå orden som används i kokböcker och recept. Men vad heter det med ett finare ord när man sjuder någonting i vätska?',
-             'answer_label': 'Pochera',
-             'answer_text': 'Pochera kommer från franska ordet poche, som betyder ficka.'},
-        20: {'title': 'Nyårstradition',
-             'question': 'Nyårsafton är omgärdat av traditioner.\n\nEn tradition som sänds på SVT varje nyår vid klockslaget är när en svensk kändis läser upp dikten Nyårsklockan (Ring klocka, ring?)\nVar i Sverige utförs denna tradition?',
-             'answer_label': 'LABEL',
-             'answer_text': 'TEXT'},
-        30: {'title': 'Nyårsdrink',
-             'question': 'Question Nyår 30',
-             'answer_label': 'LABEL',
-             'answer_text': 'TEXT'},
-        40: {'title': 'Nyårsväder',
-             'question': 'Question Nyår 40',
-             'answer_label': 'LABEL',
-             'answer_text': 'TEXT'},
-        50: {'title': 'Nyårskunskap',
-             'question': 'Question Nyår 50',
-             'answer_label': 'LABEL',
-             'answer_text': 'TEXT'},
-    },
-    'Lek på allvar': {
-        10: {'title': 'Lek på allvar',
-             'question': 'Question Lek 10',
-             'answer_label': 'LABEL',
-             'answer_text': 'TEXT'},
-        20: {'title': 'Lek på allvar',
-             'question': 'Question Lek 20',
-             'answer_label': 'LABEL',
-             'answer_text': 'TEXT'},
-        30: {'title': 'Lek på allvar',
-             'question': 'Question Lek 30',
-             'answer_label': 'LABEL',
-             'answer_text': 'TEXT'},
-        40: {'title': 'Lek på allvar',
-             'question': 'Question Lek 40',
-             'answer_label': 'LABEL',
-             'answer_text': 'TEXT'},
-        50: {'title': 'Lek på allvar',
-             'question': 'Question Lek 50',
-             'answer_label': 'LABEL',
-             'answer_text': 'TEXT'},
-    },
-    '2017': {
-        10: {'title': '2017',
-             'question': 'Question 2017 10',
-             'answer_label': 'Answer 2017 10',
-             'answer_text': 'TEXT'},
-        20: {'title': '2017',
-             'question': 'Question 2017 20',
-             'answer_label': 'Answer 2017 20',
-             'answer_text': 'TEXT'},
-        30: {'title': '2017',
-             'question': 'Question 2017 30',
-             'answer_label': 'Answer 2017 30',
-             'answer_text': 'TEXT'},
-        40: {'title': '2017',
-             'question': 'Question 2017 40',
-             'answer_label': 'Answer 2017 40',
-             'answer_text': 'TEXT'},
-        50: {'title': '2017',
-             'question': 'Question 2017 50',
-             'answer_label': 'Answer 2017 50',
-             'answer_text': 'TEXT'}
-    }}
+with open('jeopardy.json', 'r', encoding='utf-8') as json_file:
+    data = json.load(json_file)
 
 
 class Player(object):
 
     def __init__(self, name=None, points=None):
-        self.name = name
-        self.points = points
+        self._name = name
+        self._points = points
 
-    def get_name(self):
-        return self.name
+    @property
+    def name(self):
+        return self._name
 
-    def get_points(self):
-        return self.points
+    @name.setter
+    def name(self, value):
+        self._name = value
 
-    def set_name(self, name):
-        self.name = name
-        return self.name
+    @property
+    def points(self):
+        return self._points
 
-    def set_points(self, points, operation):
-        if operation == "+":
-            self.points += points
-        elif operation == "-":
-            self.points -= points
-        return self.points
+    @points.setter
+    def points(self, value):
+        self._points = value
 
     def __str__(self):
         return self.__repr__()
 
     def __repr__(self):
-        return "<Player: {}, Points: {}>".format(self.name, self.points)
-
-
-class BaseLayout(FloatLayout):
-    pass
-
-
-class WallpaperImage(Image):
-    pass
-
-
-class BoardScreen(Screen):
-    point = NumericProperty()
-
-
-class MainLayout(BoxLayout):
-    pass
-
-
-class ScoreLayout(BoxLayout):
-    pass
-
-
-class ScoreLabel(Label):
-    pass
-
-
-class PlayerLabel(Label):
-    pass
-
-
-class CategoryGrid(GridLayout):
-    pass
-
-
-class QALayout(AnchorLayout):
-    pass
-
-
-class QABox(BoxLayout):
-    pass
-
-
-class QAImage(Image):
-    pass
-
-
-class HeaderLayout(AnchorLayout):
-    pass
-
-
-class BackLayout(AnchorLayout):
-    pass
-
-
-class AnswerLayout(AnchorLayout):
-    pass
-
-
-class QuestionScreen(Screen):
-    question_text = StringProperty()
-    title = StringProperty()
-    category = StringProperty()
-    point = NumericProperty()
-
-    def __init__(self, **kwargs):
-        super(QuestionScreen, self).__init__(**kwargs)
-
-        self.name = '{category}-{point}-question'.format(
-            category=self.category, point=self.point)
-        self.app = App.get_running_app()
-
-
-class QuestionLabel(Label):
-    question_text = StringProperty()
-    font_color = StringProperty()
-
-    def __init__(self, **kwargs):
-        super(QuestionLabel, self).__init__(**kwargs)
-        self.font_color = '561f60'
-        self.markup = True
-
-    def on_size(self, *args):
-        self.canvas.before.clear()
-        boarder_size = 5
-        with self.canvas.before:
-            Color(0, 0, 0, 0.7)
-            Rectangle(pos=(self.pos[0] - boarder_size,
-                           self.pos[1] - boarder_size),
-                      size=(self.size[0] + 2 * boarder_size,
-                            self.size[1] + 2 * boarder_size))
-            Color(1, 0.75, 0.29, 0.9)
-            Rectangle(pos=self.pos, size=self.size)
-
-
-class AnswerButton(ButtonBehavior, Image):
-
-    def __init__(self, **kwargs):
-        super(AnswerButton, self).__init__(**kwargs)
-        self.source = 'media/answer.png'
-
-    def on_press(self):
-        current_screen = App.get_running_app().screen_manager.current
-        answer_screen = '-'.join(current_screen.split('-')[:2]) + '-answer'
-        App.get_running_app().screen_manager.current = answer_screen
-
-
-class SetPointsButton(ButtonBehavior, Image):
-    def __init__(self, **kwargs):
-        super(SetPointsButton, self).__init__(**kwargs)
-        self.source = 'media/set_points.png'
-
-
-class AnswerScreen(Screen):
-    answer_label = StringProperty()
-    answer_text = StringProperty()
-    title = StringProperty()
-    category = StringProperty()
-    point = NumericProperty()
-
-    def __init__(self, **kwargs):
-        super(AnswerScreen, self).__init__(**kwargs)
-
-        self.name = '{category}-{point}-answer'.format(
-            category=self.category, point=self.point)
-        self.app = App.get_running_app()
-
-
-class AnswerLabel(QuestionLabel):
-    def __init__(self, **kwargs):
-        super(AnswerLabel, self).__init__(**kwargs)
-
-
-class BackButton(ButtonBehavior, Image):
-    def __init__(self, **kwargs):
-        super(BackButton, self).__init__(**kwargs)
-        self.source = 'media/back.png'
-
-    def on_press(self):
-        App.get_running_app().screen_manager.current = 'board_screen'
-
-
-class GridLabelButton(Button):
-    font_color = StringProperty()
-    category = StringProperty()
-
-    def __init__(self, **kwargs):
-        super(GridLabelButton, self).__init__(**kwargs)
-        self.text = '[color={font_color}][b]{category}[/b][/color]'.format(
-                    category=self.category, font_color=self.font_color)
-
-
-class GridButton(Button):
-    category = StringProperty()
-    point = NumericProperty()
-    font_color = StringProperty()
-    pushed = BooleanProperty()
-
-    def __init__(self, **kwargs):
-        super(GridButton, self).__init__(**kwargs)
-        self.text = '[color={font_color}][b]{point}[/b][/color]'.format(
-                    point=self.point, font_color=self.font_color)
-        self.pushed = False
-        self.id = '{category}.{point}'.format(category=self.category, point=self.point)
-
-
-class PlayersScoreLayout(BoxLayout):
-    player = ObjectProperty()
-
-    def __init__(self, **kwargs):
-        super(PlayersScoreLayout, self).__init__(**kwargs)
-        self.app = App.get_running_app()
-        Clock.schedule_interval(self.update_points, 1)
-
-    def update_points(self, dt):
-        self.ids.score_label.text = str(self.player.get_points())
-
-
-class AddPointsPopup(Popup):
-    pass
+        return "<Player: {}, Points: {}>".format(self._name, self._points)
 
 
 class MainApp(App):
-    POINTS = JEOPARDY_DICT[next(iter(JEOPARDY_DICT.keys()))].keys()
+    POINTS = data[next(iter(data.keys()))].keys()
     point = NumericProperty()
 
     def __init__(self, **kwargs):
-        super(MainApp, self).__init__(**kwargs)
+        super().__init__(**kwargs)
         self.players = list()
         self.screen_manager = ObjectProperty()
         self.add_points_popup = ObjectProperty()
@@ -316,22 +66,22 @@ class MainApp(App):
             instance.background_color = (1, 1, 1, 0.1)
             instance.pushed = True
 
-        screen_name = instance.id.split(".")[0] + "-" + instance.id.split(".")[1] + "-question"
+        screen_name = \
+            instance.id.split(".")[0] + "-" + \
+            instance.id.split(".")[1] + "-question"
 
         self.screen_manager.current = screen_name
 
-    def close_popup(self, instance):
+    def close_popup(self, _instance):
         self.add_points_popup.dismiss()
 
     def build(self):
         self.screen_manager = self.root.ids.screen_manager
         self.screen_manager.transition = NoTransition()
-
         self.create_players()
         self.build_score_layout()
         self.build_category_grid()
         self.build_question_screens()
-        self.build_answer_screens()
         self.build_add_points_popup()
 
     def create_players(self):
@@ -340,37 +90,38 @@ class MainApp(App):
 
     def build_score_layout(self):
         for player in self.players:
-            self.root.ids.score_layout.add_widget(PlayersScoreLayout(player=player, id=player.get_name()))
+            self.root.ids.score_layout.add_widget(PlayersScoreLayout(
+                player=player, id=player.name))
 
     def build_category_grid(self):
-        for category in JEOPARDY_DICT.keys():
+        for category in data.keys():
             self.root.ids.category_grid.add_widget(GridLabelButton(
                 category=category))
 
         for point in self.POINTS:
-            for category in JEOPARDY_DICT.keys():
-                grid_button = GridButton(category=category, point=point)
+            for category in data.keys():
+                grid_button = GridButton(category=category, point=int(point))
                 grid_button.bind(on_press=self.go_to_question)
                 self.root.ids.category_grid.add_widget(grid_button)
 
     def build_question_screens(self):
-        for category in JEOPARDY_DICT.keys():
+        for category in data.keys():
             for point in self.POINTS:
-                JEOPARDY_DICT[category][point]['question_screen'] = QuestionScreen(
-                    question_text=JEOPARDY_DICT[category][point]['question'],
-                    title=JEOPARDY_DICT[category][point]['title'],
-                    category=category, point=point)
-                self.screen_manager.add_widget(JEOPARDY_DICT[category][point]['question_screen'])
-
-    def build_answer_screens(self):
-        for category in JEOPARDY_DICT.keys():
-            for point in self.POINTS:
-                JEOPARDY_DICT[category][point]['answer_screen'] = AnswerScreen(
-                    answer_label=JEOPARDY_DICT[category][point]['answer_label'],
-                    answer_text=JEOPARDY_DICT[category][point]['answer_text'],
-                    title=JEOPARDY_DICT[category][point]['title'],
-                    category=category, point=point)
-                self.screen_manager.add_widget(JEOPARDY_DICT[category][point]['answer_screen'])
+                # Build Question Screens
+                data[category][point]['question_screen'] = QuestionScreen(
+                    question_text=data[category][point]['question'],
+                    title=data[category][point]['title'],
+                    category=category, point=int(point))
+                self.screen_manager.add_widget(
+                    data[category][point]['question_screen'])
+                # Build Answer Screens
+                data[category][point]['answer_screen'] = AnswerScreen(
+                    answer_label=data[category][point]['answer_label'],
+                    answer_text=data[category][point]['answer_text'],
+                    title=data[category][point]['title'],
+                    category=category, point=int(point))
+                self.screen_manager.add_widget(
+                    data[category][point]['answer_screen'])
 
     def build_add_points_popup(self):
         popup_player_layout = PopupPlayerLayout()
@@ -386,59 +137,7 @@ class MainApp(App):
         self.add_points_popup = AddPointsPopup(content=popup_layout)
 
 
-class PopupBaseLayout(BoxLayout):
-    pass
-
-
-class PopupPlayerLayout(BoxLayout):
-    pass
-
-
-class PopupButtonLayout(BoxLayout):
-    pass
-
-
-class PopupCloseButton(Button):
-    def __init__(self, **kwargs):
-        super(PopupCloseButton, self).__init__(**kwargs)
-        self.app = App.get_running_app()
-
-    def on_press(self):
-        self.app.add_points_popup.dismiss()
-
-
-class PopupBackButton(Button):
-
-    def __init__(self, **kwargs):
-        super(PopupBackButton, self).__init__(**kwargs)
-        self.app = App.get_running_app()
-
-    def on_press(self):
-        self.app.add_points_popup.dismiss()
-        self.app.screen_manager.current = 'board_screen'
-
-
-class PopupPlayerContent(BoxLayout):
-    player = ObjectProperty()
-    points = StringProperty()
-    point = NumericProperty()
-
-    def __init__(self, **kwargs):
-        super(PopupPlayerContent, self).__init__(**kwargs)
-        self.app = App.get_running_app()
-        Clock.schedule_interval(self.set_answer_point, 0.5)
-
-    def set_answer_point(self, dt):
-        self.point = self.app.screen_manager.current_screen.point
-
-    def add_points(self, player, point):
-        player.set_points(point, "+")
-        self.ids.score_label.text = str(self.player.get_points())
-
-    def sub_points(self, player, point):
-        player.set_points(point, "-")
-        self.ids.score_label.text = str(self.player.get_points())
-
-
 if __name__ == '__main__':
+    Builder.load_file('widgets.kv')
+
     MainApp().run()
---
+++ widgets.py
@@ -0,0 +1,248 @@
+#!/usr/bin/python3
+# -*- coding: iso-8859-1 -*-
+
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.behaviors.button import ButtonBehavior
+from kivy.uix.popup import Popup
+from kivy.graphics import Color, Rectangle
+from kivy.uix.screenmanager import Screen
+from kivy.properties import StringProperty, BooleanProperty, NumericProperty, ObjectProperty
+
+
+class AddPointsPopup(Popup):
+    pass
+
+
+class AnswerButton(ButtonBehavior, Image):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.source = 'media/answer.png'
+
+    def on_press(self):
+        current_screen = App.get_running_app().screen_manager.current
+        answer_screen = '-'.join(current_screen.split('-')[:2]) + '-answer'
+        App.get_running_app().screen_manager.current = answer_screen
+
+
+class AnswerScreen(Screen):
+    answer_label = StringProperty()
+    answer_text = StringProperty()
+    title = StringProperty()
+    category = StringProperty()
+    point = NumericProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.name = '{category}-{point}-answer'.format(
+            category=self.category, point=self.point)
+        self.app = App.get_running_app()
+
+
+class AnswerLayout(AnchorLayout):
+    pass
+
+
+class BackButton(ButtonBehavior, Image):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.source = 'media/back.png'
+
+    def on_press(self):
+        App.get_running_app().screen_manager.current = 'board_screen'
+
+
+class BackLayout(AnchorLayout):
+    pass
+
+
+class BaseLayout(FloatLayout):
+    pass
+
+
+class BoardScreen(Screen):
+    point = NumericProperty()
+
+
+class CategoryGrid(GridLayout):
+    pass
+
+
+class GridLabelButton(Button):
+    font_color = StringProperty()
+    category = StringProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.text = '[color={font_color}][b]{category}[/b][/color]'.format(
+            category=self.category, font_color=self.font_color)
+
+
+class GridButton(Button):
+    category = StringProperty()
+    point = NumericProperty()
+    font_color = StringProperty()
+    pushed = BooleanProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.text = '[color={font_color}][b]{point}[/b][/color]'.format(
+            point=self.point, font_color=self.font_color)
+        self.pushed = False
+        self.id = '{category}.{point}'.format(
+            category=self.category, point=self.point)
+
+
+class HeaderLayout(AnchorLayout):
+    pass
+
+
+class MainLayout(BoxLayout):
+    pass
+
+
+class PlayerLabel(Label):
+    pass
+
+
+class PlayersScoreLayout(BoxLayout):
+    player = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.app = App.get_running_app()
+        Clock.schedule_interval(self.update_points, 1)
+
+    def update_points(self, _dt):
+        self.ids.score_label.text = str(self.player.points)
+
+
+class PopupBackButton(Button):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.app = App.get_running_app()
+
+    def on_press(self):
+        self.app.add_points_popup.dismiss()
+        self.app.screen_manager.current = 'board_screen'
+
+
+class PopupBaseLayout(BoxLayout):
+    pass
+
+
+class PopupButtonLayout(BoxLayout):
+    pass
+
+
+class PopupCloseButton(Button):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.app = App.get_running_app()
+
+    def on_press(self):
+        self.app.add_points_popup.dismiss()
+
+
+class PopupPlayerContent(BoxLayout):
+    player = ObjectProperty()
+    points = StringProperty()
+    point = NumericProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.app = App.get_running_app()
+        Clock.schedule_interval(self.set_answer_point, 0.5)
+
+    def set_answer_point(self, _dt):
+        self.point = self.app.screen_manager.current_screen.point
+
+    def add_points(self, player, point):
+        player.points += point
+        self.ids.score_label.text = str(self.player.points)
+
+    def sub_points(self, player, point):
+        player.points -= point
+        self.ids.score_label.text = str(self.player.points)
+
+
+class PopupPlayerLayout(BoxLayout):
+    pass
+
+
+class QABox(BoxLayout):
+    pass
+
+
+class QAImage(Image):
+    pass
+
+
+class QALayout(AnchorLayout):
+    pass
+
+
+class QuestionLabel(Label):
+    question_text = StringProperty()
+    font_color = StringProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.font_color = '561f60'
+        self.markup = True
+
+    def on_size(self, *_unused):
+        self.canvas.before.clear()
+        boarder_size = 5
+        with self.canvas.before:
+            Color(0, 0, 0, 0.7)
+            Rectangle(pos=(self.pos[0] - boarder_size,
+                           self.pos[1] - boarder_size),
+                      size=(self.size[0] + 2 * boarder_size,
+                            self.size[1] + 2 * boarder_size))
+            Color(1, 0.75, 0.29, 0.9)
+            Rectangle(pos=self.pos, size=self.size)
+
+
+class AnswerLabel(QuestionLabel):
+    pass
+
+
+class QuestionScreen(Screen):
+    question_text = StringProperty()
+    title = StringProperty()
+    category = StringProperty()
+    point = NumericProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.name = '{category}-{point}-question'.format(
+            category=self.category, point=self.point)
+        self.app = App.get_running_app()
+
+
+class ScoreLayout(BoxLayout):
+    pass
+
+
+class ScoreLabel(Label):
+    pass
+
+
+class SetPointsButton(ButtonBehavior, Image):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.source = 'media/set_points.png'
+
+
+class WallpaperImage(Image):
+    pass
