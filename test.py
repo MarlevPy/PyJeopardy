597e4a00c7ba29c5c356810a6fb34e6026a7e631
Marcus Levin <30433406+MarlevPy@users.noreply.github.com>
2018-01-20 01:10:08 +0100

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: iso-8859-1 -*-
-import json
-
-with open('jeopardy.json', 'r', encoding='utf-8') as json_file:
-    data = json.load(json_file)
-
-POINTS = [int(point) for point in data[next(iter(data.keys()))].keys()]
-print(POINTS)
